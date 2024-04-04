425ffc269ab7de55cd8b52071afd21e4d2882ab3
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-04-03 23:34:24 -0400

    anagrams and capitalizing words

--- chunk.js
+++ chunk.js
@@ -11,4 +11,16 @@ function chunk(arr, size) {
     return chunked
 }
 
-chunk([1, 2, 3, 4, 5], 3)
+
+function chunkTwo(arr, size) {
+    const chunked = []
+    let index = 0
+    while (index < arr.length) {
+        chunked.push(arr.slice(index, index + size))
+        index += size
+    }
+    return chunked
+}
+
+
+chunkTwo([1, 2, 3, 4, 5], 2)
